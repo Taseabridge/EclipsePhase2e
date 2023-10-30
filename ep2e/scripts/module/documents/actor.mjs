a26bba0150aebbe071c89eef0faa4fe6fa1a73a3
Erik Itter <erik.itter@gmail.com>
2023-10-30 19:20:44 +0100

    ...

---
+++ ep2e/scripts/module/documents/actor.mjs
@@ -0,0 +1,39 @@
+export default class ActorEp2e extends Actor {
+
+    constructor(options = {}) {
+        super(options);
+
+        this.morphSleeved = null;
+    }
+
+    get armor() {
+        return { e: 0, k: 0, items: [] };
+    }
+
+    /**
+     * returns {object} Ego.
+     */
+    get ego() {
+
+    }
+
+    /**
+     * returns {object} Array of Morph, empty Array if not having any.
+     */
+    get morphs() {
+
+    }
+
+    get movement() {
+
+    }
+
+    /**
+     * @param {Morph} morph Morph to be sleeved.
+     */
+    set morphSleeved(morph) {
+        this.morphSleeved = morph;
+    }
+
+
+}
