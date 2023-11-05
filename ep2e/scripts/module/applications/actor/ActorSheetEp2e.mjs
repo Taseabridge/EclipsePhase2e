dadf7cec55c20503dc674363b9091de416892a8d
Tyler Seabridge <taseabridge@gmail.com>
2023-11-05 15:16:55 -0500

    actor-sheet

--- ep2e/scripts/module/applications/actor/ActorSheetEp2e.mjs
+++ ep2e/scripts/module/applications/actor/ActorSheetEp2e.mjs
@@ -1,4 +1,20 @@
 export default class ActorSheetEp2e extends ActorSheet {
 
+    /** @override */
+    get template() {
+        return "systems/ep2e/templates/actor-sheet.html";
+    }
 
+    /* -------------------------------------------- */
+    /*  Context Preparation                         */
+    /* -------------------------------------------- */
+
+    /** @override */
+    async getData(options) {
+
+        // The Actor's data
+        const source = this.actor.toObject();
+
+
+    }
 }
