d61e48c6d7178c8e085a0c43e818323cf6bf0532
Tyler Seabridge <taseabridge@gmail.com>
2023-11-05 15:43:38 -0500

    onDelete

--- ep2e/scripts/module/applications/actor/ActorSheetEp2e.mjs
+++ ep2e/scripts/module/applications/actor/ActorSheetEp2e.mjs
@@ -17,4 +17,41 @@ export default class ActorSheetEp2e extends ActorSheet {
 
 
     }
+
+    activateListeners(html) {
+        // Editable Only Listeners
+        if (this.isEditable) {
+            const inputs = html.find("input");
+
+            // Owned Item management
+            html.find(".item-delete").click(this._onItemDelete.bind(this));
+
+
+        }
+
+    }
+
+    /**
+      * Handle deleting an existing Owned Item for the Actor.
+      * @param {Event} event  The originating click event.
+      * @returns {Promise<ItemEP2e|AdvancementManager>|undefined}  The deleted item if something was deleted or the
+      *                                                          advancement manager if advancements need removing.
+      * @private
+      */
+    async _onItemDelete(event) {
+        event.preventDefault();
+        const li = event.currentTarget.closest(".item");
+        const item = this.actor.items.get(li.dataset.itemId);
+        if (!item) return;
+
+
+        return item.deleteDialog();
+    }
+
+    /* -------------------------------------------- */
+
+
+
+
+
 }
