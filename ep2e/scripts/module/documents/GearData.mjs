b044d7bac36aa8e4595de81b8fe4915e163a15bc
Erik Itter <erik.itter@gmail.com>
2023-11-06 05:24:50 +0100

    minor model progress and project structure

--- ep2e/scripts/module/documents/GearData.mjs
+++ ep2e/scripts/module/documents/GearData.mjs
@@ -1,19 +1,56 @@
 import ItemData from "./ItemData.mjs";
 
+/**
+ * @todo ActiveEffects, probably on ItemData
+ */
 export default class GearData extends ItemData {
 
     static defineSchema() {
-        const fields = foundry.data.fields;
 
-        const composite = super.defineSchema(arguments);
+        const item = super.defineSchema();
 
-        composite.gp = new fields.NumberField({
+        const gear = {};
+
+        /**
+         * Gear Point value.
+         */
+        gear.gp = new foundry.data.fields.NumberField({
             required: true,
             initial: 0,
             integer: true
         });
 
-        return composite;
+        /**
+         * Gear Complexity.
+         * 
+         * @todo make sure it can only be one of the rules defined ones.
+         */
+        gear.complexity = new foundry.data.fields.StringField({
+            required: true,
+            initial: ""
+        });
+
+        const quality = {};
+
+        /**
+         * The value of the bonus granted or malus imposed (pg. 311).
+         */
+        quality.quality = new foundry.data.fields.NumberField({
+            required: true,
+            initial: 0,
+            integer: true
+        });
+
+        /**
+         * Description how the quality of this item differs from normal.
+         */
+        quality.description = new foundry.data.fields.HTMLField();
+
+        gear.quality = new foundry.data.fields.SchemaField(quality);
+
+        item.gear = new foundry.data.fields.SchemaField(gear);
+
+        return item;
     }
 }
 
