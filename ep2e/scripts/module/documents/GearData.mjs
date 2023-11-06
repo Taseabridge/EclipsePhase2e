9cd3d99fbbc350863982d3c3647c12b4da24a7e9
Erik Itter <erik.itter@gmail.com>
2023-11-06 05:43:07 +0100

    more modelling experimentation (progress)

--- ep2e/scripts/module/documents/GearData.mjs
+++ ep2e/scripts/module/documents/GearData.mjs
@@ -30,6 +30,14 @@ export default class GearData extends ItemData {
             initial: ""
         });
 
+        /**
+         * @description To hide an object from others, use Infiltrate or Exotic Skill: Sleight of Hand skill. This is opposed by Perceive. Objects with the Concealable trait add a +30 modifier. If the searcher is conducting a thorough physical search, treat as a task action and apply a modifier if they take extra time ▶32. (pg. 311)
+         */
+        gear.concealable = new foundry.data.fields.BooleanField({
+            required: true,
+            initial: false
+        });
+
         const quality = {};
 
         /**
@@ -47,6 +55,28 @@ export default class GearData extends ItemData {
         quality.description = new foundry.data.fields.HTMLField();
 
         gear.quality = new foundry.data.fields.SchemaField(quality);
+
+
+        /**
+         * @description Most technology is wirelessly meshed with other devices around it (The Mesh ▶240). They contain a miniaturized computer, a wireless transceiver, and possibly laser or microwave links and/or various sensors. They can be tracked via the mesh ▶256, and are vulnerable to hacking and intrusion attempts, though protected by their firewall ▶260 and device ALI ▶250. For privacy and security, most people slave their carried and worn devices that make up their personal area network (PAN) to their mesh inserts or ecto (Slaved Devices ▶241). The Radio and Sensor Ranges table notes what range these devices operate at. (pg. 311)
+         */
+        const mesh = {};
+        mesh.isMeshed = new foundry.data.fields.BooleanField({
+            required: true,
+            initial: true
+        });
+        mesh.radio = new foundry.data.fields.NumberField({
+            required: true,
+            initial: 0,
+            integer: true
+        });
+        mesh.sensor = new foundry.data.fields.NumberField({
+            required: true,
+            initial: 0,
+            integer: true
+        });
+        gear.mesh = new foundry.data.fields.SchemaField(mesh);
+
 
         item.gear = new foundry.data.fields.SchemaField(gear);
 
