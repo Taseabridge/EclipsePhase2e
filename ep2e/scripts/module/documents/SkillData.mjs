dff91d18aad722d485050c78beec425b432c4fca
Erik Itter <erik.itter@gmail.com>
2023-11-06 12:48:40 +0100

    changed skill's types to categories as per rules, not type system types

--- ep2e/scripts/module/documents/SkillData.mjs
+++ ep2e/scripts/module/documents/SkillData.mjs
@@ -3,27 +3,32 @@ import ItemData from "./ItemData.mjs";
 export default class SkillData extends ItemData {
     static defineSchema() {
 
-        const skill = super.defineSchema(arguments);
+        const item = super.defineSchema(arguments);
 
-        skill.skill = new foundry.data.fields.SchemaField({
+        const skill = {};
 
-            aptitude: new foundry.data.fields.StringField({
-                required: true,
-                initial: ""
-            }),
 
-            rank: new foundry.data.fields.NumberField({
-                required: true,
-                initial: 0,
-                integer: true
-            }),
-
-            types: new foundry.data.fields.ArrayField(
-                new foundry.data.fields.StringField({
-                })
-            )
+        skill.aptitude = new foundry.data.fields.StringField({
+            required: true,
+            initial: ""
         });
 
-        return skill;
+        skill.rank = new foundry.data.fields.NumberField({
+            required: true,
+            initial: 0,
+            integer: true
+        });
+
+        /**
+         * @description Skills are also assigned to a number of other categories, as noted on the Skill List ▶next page: Combat, Mental, Physical, Psi, Social, Technical, and Vehicle skills. Certain traits, abilities, and modifiers may only apply to skills of a specific category. (pg. 48)
+         */
+        skill.categories = new foundry.data.fields.ArrayField(
+            new foundry.data.fields.StringField({
+            })
+        );
+
+        item.skill = new foundry.data.fields.SchemaField(skill);
+
+        return item;
     }
 }
