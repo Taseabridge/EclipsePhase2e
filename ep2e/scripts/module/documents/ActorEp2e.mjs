44803413c6586a93f440c92babad56b291ea9e75
Erik Itter <erik.itter@gmail.com>
2023-11-06 23:08:17 +0100

    added empty overwrite for preparing data (derived), issue #9

--- ep2e/scripts/module/documents/ActorEp2e.mjs
+++ ep2e/scripts/module/documents/ActorEp2e.mjs
@@ -1,3 +1,15 @@
 export default class ActorEp2e extends Actor {
 
+    /**
+     * 
+     * @todo Remove log, or if fine grained logging control has been implemented change it for the appropriate tracing or debugging solution.
+     * @inheritdoc
+     */
+    prepareDerivedData() {
+        console.log({ message: `ActorEp2e.prepareDerivedData()`, context: this, arguments: arguments });
+
+        // prepare derived data
+        // process the items
+        // lots of stuf, this is arguably one of the most important places to work on, besides further vertical exploration in order to discover stuff we need.
+    }
 }
