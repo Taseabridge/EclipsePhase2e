45e76d333bbdf1d04a5e1c64f40b3f9fca566fd5
Erik Itter <erik.itter@gmail.com>
2023-11-06 06:05:17 +0100

    panning, issue #18

---
+++ ep2e/scripts/module/diceEp2e.mjs
@@ -0,0 +1,22 @@
+/**
+ * Provides all the dice rolling logic and delegates what is possible to the appropriate Foundry tools.
+ */
+export default class DiceEp2e {
+
+    /**
+     * 
+     * @todo to roll 1d100 or actually do as the book suggests for the table?
+     * @todo data structure for holding the result for further mutation, like Flex?
+     */
+    async rollCheck() {
+
+    }
+
+    /**
+     * 
+     * @param {Object} rolldata 
+     */
+    async rollDamage(rolldata = {}) {
+
+    }
+}
