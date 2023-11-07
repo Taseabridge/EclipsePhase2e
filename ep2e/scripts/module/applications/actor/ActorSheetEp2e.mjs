c0043c8a8fd1cecd1825162be84aea72c15b0a32
Tyler Seabridge <taseabridge@gmail.com>
2023-11-06 23:39:14 -0500

    asd

--- ep2e/scripts/module/applications/actor/ActorSheetEp2e.mjs
+++ ep2e/scripts/module/applications/actor/ActorSheetEp2e.mjs
@@ -1,5 +1,17 @@
 export default class ActorSheetEp2e extends ActorSheet {
 
+    static get defaultOptions() {
+        return foundry.utils.mergeObject(super.defaultOptions, {
+            classes: ["worldbuilding", "sheet", "actor"],
+            template: "systems/worldbuilding/templates/actor-sheet.html",
+            width: 600,
+            height: 600,
+            tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description" }],
+            scrollY: [".biography", ".items", ".attributes"],
+            dragDrop: [{ dragSelector: ".item-list .item", dropSelector: null }]
+        });
+    }
+
     /** @override */
     get template() {
         return "systems/ep2e/templates/actor-sheet.html";
@@ -13,7 +25,7 @@ export default class ActorSheetEp2e extends ActorSheet {
     async getData(options) {
 
         // The Actor's data
-        const actor = this.actor.toObject();
+        const source = this.actor.toObject();
         console.log({ message: 'getData', source: source });
 
     }
