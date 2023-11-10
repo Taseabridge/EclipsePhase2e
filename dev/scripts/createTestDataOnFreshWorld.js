a93911eb643c7161e22951e7b03236b72c846bea
Erik Itter <erik.itter@gmail.com>
2023-11-10 21:21:05 +0100

    ...

--- dev/scripts/createTestDataOnFreshWorld.js
+++ dev/scripts/createTestDataOnFreshWorld.js
@@ -1,5 +1,8 @@
 await Actor.create({ name: "EGR_2.71828", type: "character" });
+actor = game.actors.getName("EGR_2.71828");
 await Item.create({ name: "EGR_2.71828", type: "ego" });
-await game.actors.getName("EGR_2.71828").createEmbeddedDocuments("Item", [game.items.getName("EGR_2.71828")]);
-await game.actors.getName("EGR_2.71828").update({ "system.ego.egos": game.actors.getName("EGR_2.71828").items.filter(i => i.type === "ego").map(i => i.id) });
-await game.actors.getName("EGR_2.71828").update({ "system.ego.currentId": game.actors.getName("EGR_2.71828").system.ego.egos[0] });
+item = game.items.getName("EGR_2.71828");
+await item.update({ "system.aptitudes": { cognition: 30, intuition: 30, reflexes: 5, savvy: 5, somatics: 5, willpower: 10 } });
+await actor.createEmbeddedDocuments("Item", [game.items.getName("EGR_2.71828")]);
+await actor.update({ "system.ego.egos": actor.items.filter(i => i.type === "ego").map(i => i.id) });
+await actor.update({ "system.ego.currentId": actor.system.ego.egos[0] });
--- ep2e/scripts/module/documents/EgoData.mjs
+++ ep2e/scripts/module/documents/EgoData.mjs
@@ -5,6 +5,9 @@ export default class EgoData extends ItemData {
 
         const ego = super.defineSchema();
 
+        /**
+         * If we go full changes with templates and changes after creation this would be changed into cached data prepared on ActorEp2e.
+         */
         ego.aptitudes = new foundry.data.fields.SchemaField({
             cognition: new foundry.data.fields.NumberField({
                 required: true,
@@ -38,6 +41,17 @@ export default class EgoData extends ItemData {
             })
         });
 
+        /**
+         * Ids of the change Items accquired by this Ego. If there can be multiple Ego on an Actor (not sure that was a good idea though), and we want to track and construct from changes the change needs to be associated to the Ego. With one Ego items.filter(i=>i.type==="change") or something like it would do. I wonder if the changes should just be ActiveEffect.
+         */
+        ego.changeIds = new foundry.data.fields.ArrayField(
+            new foundry.data.fields.StringField({
+            })
+        );
+
+        /**
+         * If we really do the changes this would be replaced with caching on ActorEp2e with prepared data.
+         */
         ego.skills = new foundry.data.fields.ArrayField(
             new foundry.data.fields.StringField({
             })
